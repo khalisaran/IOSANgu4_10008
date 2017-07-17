355218226085d20c29f0328bef71cd40db044203
DHINAKAR-P <dhinakarintech@gmail.com>
2017-07-17 20:46:10 +0530

    mac mini password changes

--- projects/mobile/ios/exec_tablet.sh
+++ projects/mobile/ios/exec_tablet.sh
@@ -1,6 +1,6 @@
-export PATH=$PATH:/Users/administrator/gradle-2.11/bin
+#export PATH=$PATH:/Users/administrator/gradle-2.11/bin
 echo 'Gradle Path set..!'
-echo 3NCQCqv4 | sudo -S chmod -R 777 ~/Desktop/IOSANgu4_10008_tablet/
+echo welcome2Pwc | sudo -S chmod -R 777 ~/Desktop/IOSANgu4_10008_tablet/
 cd ~/Desktop/IOSANgu4_10008_tablet/
 pwd
 echo 'Started to excute gradle script'
